181fb42a0935625a7dcb06aa2ca72dc2f70f8bab
Fabio Menozzi <f.menozzi@yahoo.com>
2020-07-16 23:14:40 -0300

    Done

--- restapi/api/views.py
+++ restapi/api/views.py
@@ -1,4 +1,23 @@
 
 
+from collections import Counter
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+@api_view(['POST'])
 def lambda_function(request):
-    raise NotImplementedError
+    request_data = request.data.get('question')
+    
+    counter = Counter()
+    for num in request_data:
+        counter[num] += 1
+
+    solution = []
+
+    for i in counter.most_common():
+        for _ in range(i[1]):
+            solution.append(i[0])
+    print(solution)
+
+    return Response({'solution': solution})
